85b0b8bb0d41adfad6bd962cc7344d76390b4605
App Academy Student <student@appacademy.io>
2016-11-25 11:54:48 -0800

    Phase2 completed

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -1,14 +1,24 @@
+require 'prime'
+
 class Fixnum
   # Fixnum#hash already implemented for you
 end
 
 class Array
+
   def hash
+    return 0 if self.empty?
+    prime_list = Prime.first(self.length)
+    map.with_index do |el, i|
+      el * prime_list[i]
+    end.inject(:+)
   end
 end
 
 class String
   def hash
+    return 0 if length.zero?
+    self.chars.map(&:ord).hash
   end
 end
 
@@ -16,6 +26,9 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    return 0 if keys.empty?
+    keys_arr = self.keys.map(&:to_s).sort.map(&:ord)
+    values_arr = self.values.map(&:to_s).sort.map(&:ord)
+    (keys_arr + values_arr).hash
   end
 end
