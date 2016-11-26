ab4c677cbd61a414201be08422e8dd7e6739470d
App Academy Student <student@appacademy.io>
2016-11-25 16:36:06 -0800

    Hash map is done

--- lib/p05_hash_map.rb
+++ lib/p05_hash_map.rb
@@ -1,7 +1,9 @@
 require_relative 'p02_hashing'
 require_relative 'p04_linked_list'
-
+require 'byebug'
 class HashMap
+  include Enumerable
+
   attr_reader :count
 
   def initialize(num_buckets = 8)
@@ -10,18 +12,35 @@ class HashMap
   end
 
   def include?(key)
+    @store[bucket(key)].include?(key)
   end
 
   def set(key, val)
+    resize! if @count == num_buckets
+    if include?(key)
+      @store[bucket(key)].update(key, val)
+    else
+      @store[bucket(key)].append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    @store[bucket(key)].get(key)
   end
 
   def delete(key)
+    @store[bucket(key)].remove(key)
+    @count -= 1
   end
 
-  def each
+  def each(&prc)
+    @store.length.times do |i|
+      @store[i].each do |link|
+        prc.call(link.key, link.val) if block_given?
+      end
+    end
+
   end
 
   # uncomment when you have Enumerable included
@@ -42,16 +61,18 @@ class HashMap
   end
 
   def resize!
-    temp = @store.flatten
-    @store = Array.new(num_buckets * 2) { Array.new }
+    temp = @store
+    @store = Array.new(num_buckets * 2) { LinkedList.new }
     @count = 0
-    # debugger
-    temp.each do |num|
-      self.insert(num)
+    temp.each do |bucket|
+      bucket.each do |link|
+        self.set(link.key, link.val)
+      end
     end
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    key.hash % num_buckets
   end
 end
